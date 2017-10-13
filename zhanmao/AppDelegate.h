09bd843e1a5e42db22c552513b088afa3d3468f2
bangju <bangju@bangjudemacbook-pro.local>
2017-10-13 11:11:11 +0800

    Initial Commit

---
+++ zhanmao/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zhanmao
+//
+//  Created by bangju on 2017/10/13.
+//  Copyright © 2017年 bangju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
